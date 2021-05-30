461b6f66edb22fa4ab4faae64ff16e2c512149e0
JoyDas <joydascsepuc@gmail.com>
2021-05-30 11:07:20 +0600

    Add Time & Author

--- index.php
+++ index.php
@@ -5,10 +5,10 @@
 			while (have_posts()): the_post(); ?>
 				<h3><?php the_title() ?></h3>
 				<small>Posted By: <?php the_author(); ?></small>
-				<small>Posted On: <?php the_time(); ?> in <?php the_category();?></small>
+				<small>Posted On: <?php the_time('F j, Y'); ?>  <?php the_time('g:i a'); ?> in <?php the_category();?></small>
 				<p><?php the_content() ?></p><?php 
 			endwhile;
 		endif;
 	?>
-
+	
 <?php get_footer(); ?>
