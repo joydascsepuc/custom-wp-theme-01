4e8e199cb8ad6b2091e7082239da3979e427f630
JoyDas <joydascsepuc@gmail.com>
2021-05-30 10:40:13 +0600

    Add Author

--- index.php
+++ index.php
@@ -3,7 +3,9 @@
 	<?php
 		if(have_posts()):
 			while (have_posts()): the_post(); ?>
-				<h1><?php the_title() ?></h1>
+				<h3><?php the_title() ?></h3>
+				<small>Posted By: <?php the_author(); ?></small>
+				<small>Posted On: <?php the_time(); ?> in <?php the_category();?></small>
 				<p><?php the_content() ?></p><?php 
 			endwhile;
 		endif;
